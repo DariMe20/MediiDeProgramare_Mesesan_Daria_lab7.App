b0122917216eaeebec5855fe672af49ee387dcde
Daria <dariamesesan@yahoo.com>
2023-11-26 11:12:09 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MediiDeProgramare_Mesesan_Daria_lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MediiDeProgramare_Mesesan_Daria_lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
